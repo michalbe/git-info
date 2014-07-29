f73d8342f56543b2b467abf1add318c69c49dd1f
Michal Budzynski <michal@virtualdesign.pl>
2014-07-30 01:32:15 +0200

    [GIT-INFO] - :-1: tests

--- tests/git-info-tests.js
+++ tests/git-info-tests.js
@@ -1,12 +1,33 @@
 'use strict';
 
+// I hope those tests will work also in forks.
+// I hope.
+// Hope is mother of stupid
 var assert = require('assert');
 var gi = require('../');
 
 // First argument as a string
 gi('name', function(err, result) {
   assert(!err);
-  assert(!Array.isArray(result));
   assert('name' in result);
+  assert(!Array.isArray(result.name));
   assert.equal(result.name, 'git-info');
 });
+
+// First argument as an Array
+gi(['name', 'repository'], function(err, result) {
+  assert(!err);
+  assert('name' in result);
+  assert('repository' in result);
+  assert.equal(result.name, 'git-info');
+});
+
+// Multiline answer
+gi('authors', function(err, result) {
+  assert(!err);
+  assert('authors' in result);
+  assert(Array.isArray(result.authors));
+  assert(~result.authors.indexOf('Michal Budzynski <michal@virtualdesign.pl>'));
+});
+
+// On the other hand - 'Hope is when you feel the pain that makes you try again'
